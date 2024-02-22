891f5ebbca8542c0ef38c0831b4f535dcfc461f8
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-02-22 09:10:05 +0100

    Added DB connection

---
+++ src/main/java/com/fincons/FinconsNfcLearningBeApplication.java
@@ -0,0 +1,13 @@
+package com.fincons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinconsNfcLearningBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinconsNfcLearningBeApplication.class, args);
+	}
+
+}
