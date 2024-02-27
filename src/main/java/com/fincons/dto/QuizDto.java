cb53314c94f327be9d3ccaf32d76f3a13f709f02
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-02-27 10:32:54 +0100

    Added auditing for Quiz

--- src/main/java/com/fincons/dto/QuizDto.java
+++ src/main/java/com/fincons/dto/QuizDto.java
@@ -1,11 +1,17 @@
 package com.fincons.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -23,4 +29,13 @@ public class QuizDto {
 
     @JsonIgnoreProperties("quiz")
     private LessonDto lesson;
+
+    //AUDITING
+    private LocalDateTime createDate;
+
+    private LocalDateTime lastModified;
+
+    private String createdBy;
+
+    private String lastModifiedBy;
 }
