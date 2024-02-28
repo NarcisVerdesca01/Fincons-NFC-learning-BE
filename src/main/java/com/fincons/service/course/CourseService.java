74ae1881e23bed0da4f0806fad8be505732892f0
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-02-28 11:39:45 +0100

    Fixed control for backGround image

--- src/main/java/com/fincons/service/course/CourseService.java
+++ src/main/java/com/fincons/service/course/CourseService.java
@@ -57,7 +57,7 @@ public class CourseService implements ICourseService {
     @Override
     public Course createCourse(CourseDto courseDto) throws CourseException {
 
-        if (StringUtils.isBlank(courseDto.getName()) || StringUtils.isBlank(courseDto.getDescription()) || StringUtils.isBlank(courseDto.getDescription())) {
+        if (StringUtils.isBlank(courseDto.getName()) || StringUtils.isBlank(courseDto.getDescription()) || StringUtils.isBlank(courseDto.getBackgroundImage())) {
             throw new CourseException("Name, description or requirements not present");
         }
         if (courseRepository.existsByName(courseDto.getName())) {
