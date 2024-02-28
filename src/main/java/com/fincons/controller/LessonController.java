d5423ac8dd6a6ae727ea702b913cb8899e2f7d19
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-02-28 11:50:45 +0100

    Fixed Service about findallLesson

--- src/main/java/com/fincons/controller/LessonController.java
+++ src/main/java/com/fincons/controller/LessonController.java
@@ -1,9 +1,7 @@
 package com.fincons.controller;
 
-import com.fincons.dto.CourseDto;
 import com.fincons.dto.LessonDto;
 import com.fincons.mapper.LessonMapper;
-import com.fincons.service.content.IContentService;
 import com.fincons.service.lesson.ILessonService;
 import com.fincons.utility.ApiResponse;
 import lombok.AllArgsConstructor;
@@ -17,37 +15,45 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
-import com.fincons.mapper.CourseMapper;
-import com.fincons.service.course.ICourseService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("${application.context}")
 public class LessonController {
 
-   @Autowired
-   private ILessonService iLessonService;
+    @Autowired
+    private ILessonService iLessonService;
 
-   @Autowired
-   private LessonMapper lessonMapper;
+    @Autowired
+    private LessonMapper lessonMapper;
 
-    @GetMapping("${course.get-all-lessons}")
-    public ResponseEntity<ApiResponse<List<LessonDto>>> getAllLessons(){
-        List<LessonDto> lessonDtoList= iLessonService.findAllLessons()
+    @GetMapping("${lesson.get-all-lessons}")
+    public ResponseEntity<ApiResponse<List<LessonDto>>> getAllLessons() {
+        List<LessonDto> lessonDtoList = iLessonService.findAllLessons()
                 .stream()
-                .map(s->lessonMapper.mapLessonToLessonDto(s))
+                .map(s -> lessonMapper.mapLessonToLessonDto(s))
                 .toList();
         return ResponseEntity.ok().body(ApiResponse.<List<LessonDto>>builder()
                 .data(lessonDtoList)
                 .build());
     }
 
+
+    /*
+    @GetMapping("${lesson.getById}/{id}")
+    public ResponseEntity<ApiResponse<LessonDto>> getLessonById(@PathVariable long id){
+        try{
+            LessonDto lessonDtoToShow = lessonMapper.mapLessonToLessonDto(iLessonService.findLessonById(id));
+            return ResponseEntity.ok().body(ApiResponse.<LessonDto>builder()
+                    .data(lessonDtoToShow)
+                    .build());
+        }catch(ResourceNotFoundException resourceNotFoundException){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.<LessonDto>builder()
+                    .message(resourceNotFoundException.getMessage())
+                    .build());
+        }
+    }
+*/
 
 
 }
